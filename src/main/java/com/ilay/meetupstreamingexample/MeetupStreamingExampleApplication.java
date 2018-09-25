ec3f42fdbad1126807ad736de55a2be87b5ddfb7
Ilay <sakhno.iv@gmail.com>
2018-09-26 00:31:58 +0300

    Initial commit

---
+++ src/main/java/com/ilay/meetupstreamingexample/MeetupStreamingExampleApplication.java
@@ -0,0 +1,12 @@
+package com.ilay.meetupstreamingexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetupStreamingExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MeetupStreamingExampleApplication.class, args);
+    }
+}
